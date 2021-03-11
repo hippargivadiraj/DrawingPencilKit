9880536f37ea5e955bbb980933b07158ddb411dd
Vadiraj Hippargi <Vadi@leadconsultant.com>
2021-03-10 20:18:46 -0800

    Initial Commit

---
+++ DrawingPencilKit/DrawingPencilKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingPencilKitApp.swift
+//  DrawingPencilKit
+//
+//  Created by Vadiraj Hippargi on 3/10/21.
+//
+
+import SwiftUI
+
+@main
+struct DrawingPencilKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
