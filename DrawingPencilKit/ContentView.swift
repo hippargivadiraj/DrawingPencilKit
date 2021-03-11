fe49624fa2b81ff677ff1c344deed4fa52a955a8
Vadiraj Hippargi <Vadi@leadconsultant.com>
2021-03-10 20:38:29 -0800

    Using Pencil Kit is SWIFTUI

--- DrawingPencilKit/ContentView.swift
+++ DrawingPencilKit/ContentView.swift
@@ -6,13 +6,45 @@
 //
 
 import SwiftUI
+import PencilKit
 
 struct ContentView: View {
+    
+    @Environment(\.undoManager) private var undoManager
+    @State private var canwasView = PKCanvasView()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 10) {
+            Button("Clear"){
+                canwasView.drawing = PKDrawing()
+            }
+            Button("Undo"){
+                undoManager?.undo()
+            }
+            Button("Redo"){
+                undoManager?.redo()
+            }
+            
+            MyCanvas(canvasView: $canwasView)
+        }
     }
 }
+
+
+struct MyCanvas: UIViewRepresentable {
+    @Binding var canvasView: PKCanvasView
+    
+    func makeUIView(context: Context) -> PKCanvasView {
+        canvasView.drawingPolicy = .anyInput
+        canvasView.tool = PKInkingTool(.pen, color: .black, width: 15)
+        return canvasView
+    }
+    
+    func updateUIView(_ uiView: PKCanvasView, context: Context) {
+        
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
